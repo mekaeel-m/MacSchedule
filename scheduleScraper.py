530107c8e3f3b520c8a91423951cedaac8668672
mochai_m <mekaeel.r.malik@gmail.com>
2026-01-08 14:44:44 -0500

    Implemented intial calendar event functionality

--- scheduleScraper.py
+++ scheduleScraper.py
@@ -5,16 +5,150 @@ from matplotlib import pyplot as plt
 import numpy as np
 from PIL import Image
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, url_for
+from flask_session import Session
 import os
 import shutil
 from flask_cors import CORS
 
+local_url = "http://127.0.0.1:5000"
+web_url = "http://127.0.0.1:8080"
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={r"/*": {"origins": [web_url,local_url]}})
+
+app.config["SESSION_PERMANENT"] = False     # Sessions expire when the browser is closed
+app.config["SESSION_TYPE"] = "filesystem"     # Store session data in files
+
+# Initialize Flask-Session
+Session(app)
 
 UPLOAD_FOLDER = 'assets/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+#Flask Endpoints
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    erase_folder_contents('assets/')
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    file_path = os.path.join(UPLOAD_FOLDER, 'sched.png')
+    file.save(file_path)
+
+    return output()
+
+
+
+# Google login
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
+
+@app.route('/authorizeurl')
+def authorize():
+    # Required, call the from_client_secrets_file method to retrieve the client ID from a
+    # client_secret.json file. The client ID (from that file) and access scopes are required. (You can
+    # also use the from_client_config method, which passes the client configuration as it originally
+    # appeared in a client secrets file but doesn't access the file itself.)
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('client_secret.json',
+        scopes=['https://www.googleapis.com/auth/calendar.events.owned'])
+
+    # Required, indicate where the API server will redirect the user after the user completes
+    # the authorization flow. The redirect URI is required. The value must exactly
+    # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+    # configured in the API Console. If this value doesn't match an authorized URI,
+    # you will get a 'redirect_uri_mismatch' error.
+    flow.redirect_uri = local_url + '/signon'
+
+    # Generate URL for request to Google's OAuth 2.0 server.
+    # Use kwargs to set optional request parameters.
+    authorization_url, state = flow.authorization_url(
+        # Recommended, enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Optional, enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true',
+        # Optional, if your application knows which user is trying to authenticate, it can use this
+        # parameter to provide a hint to the Google Authentication Server.
+        #login_hint='hint@example.com',
+        # Optional, set prompt to 'consent' will prompt the user for consent
+        prompt='consent')
+
+    return (authorization_url)
+
+@app.route('/signon')
+def signon():
+    print(request.args.get('state'))
+    # Allowing localhost HTTP for OAuthlib. In production, use HTTPS.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+ 
+    
+    state = request.args.get('state')
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'client_secret.json',
+        scopes=['https://www.googleapis.com/auth/calendar.events.owned'],
+        state=state)
+    flow.redirect_uri = url_for('signon', _external=True)
+
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store the credentials in browser session storage, but for security: client_id, client_secret,
+    # and token_uri are instead stored only on the backend server.
+    credentials = flow.credentials
+
+    print(credentials.granted_scopes)
+
+    session_credentials = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'granted_scopes': credentials.granted_scopes}
+    
+    test_add_events(session_credentials)
+    
+    return "worked"
+
+
+def test_add_events(session_credentials):
+    from googleapiclient.discovery import build
+    import google.oauth2.credentials
+
+    credentials = google.oauth2.credentials.Credentials(
+        **session_credentials,
+        client_id='YOUR_CLIENT_ID',
+        client_secret='YOUR_CLIENT_SECRET',
+        token_uri='https://oauth2.googleapis.com/token')
+
+    service = build('calendar', 'v3', credentials=credentials)
+
+    event = {
+      'summary': 'Test Event',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2024-07-01T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'dateTime': '2024-07-01T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
+# Image Processing and OCR
 
 def extract_text_from_image(image_path):
     # Load original
@@ -158,21 +292,6 @@ def erase_folder_contents(folder_path):
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    erase_folder_contents('assets/')
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    file_path = os.path.join(UPLOAD_FOLDER, 'sched.png')
-    file.save(file_path)
-
-    return output()
 
 def output():
 
